2589514f704a61cf9c5a095b01823350610cde19
Shubham Sharma <shubhamgetmail@gmail.com>
2025-11-17 18:58:26 +0530

    changed endpoint

--- src/TVChart.jsx
+++ src/TVChart.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { widget } from "./charting_library";
 
-const API_BASE = "http://localhost:8000/api";
+const API_BASE = "http://91.203.134.194:8000/api";
 
 // Fetch all strategies
 async function fetchStrategies() {
